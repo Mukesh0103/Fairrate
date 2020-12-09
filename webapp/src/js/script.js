c3a501a815065b2afbb81a6348a7c33706d54e9d
Mukesh0103 <mukeshkannan@users.noreply.github.com>
2020-12-09 20:39:32 +0530

    Updated

--- webapp/src/js/script.js
+++ webapp/src/js/script.js
@@ -1,10 +1,34 @@
 var screen1 = document.getElementById("screen1");
-var screen2 = document.getElementById("screen2")
-var screen3 = document.getElementById("screen3")
+var screen2 = document.getElementById("screen2");
 var nextPage = document.getElementById("next-btn");
-nextPage.addEventListener('click', function() {
-    if(getComputedStyle(screen2).display === "none") {
-        screen2.style.display = "block";
-        screen1.style.display = "none";
+nextPage.addEventListener('click', function () {
+    getComputedStyle(screen2).display === "none" ? (screen2.style.display = "block") && (screen1.style.display = "none") : (screen1.style.display = "block");
+})
+
+
+var selectDropdown = document.getElementById("select-dropdown");
+var selectInput = document.getElementById("select-input");
+selectInput.addEventListener('click', function () {
+    if (getComputedStyle(selectDropdown).display === "none") {
+        selectDropdown.style.display = "block";
     }
 })
+
+var dropdownList = document.getElementsByClassName("dropdown-list");
+for (var i = 0; i < dropdownList.length; i++) {
+    var current = dropdownList[i];
+    current.addEventListener('click', function () {
+        selectInput.innerHTML = event.target.innerHTML;
+        selectDropdown.style.display = "none";
+    });
+}
+
+// var profileDetails = document.getElementById("screen2");
+// var inputCheckbox = document.getElementsByClassName("checkbox-container");
+// inputCheckbox.addEventListener('change', function(){
+//     if(inputCheckbox.checked === true) {
+//         profileDetails.classList.add("is-selected");
+//     } else {
+//         profileDetails.classList.remove("is-selected")   
+//     }
+// })
