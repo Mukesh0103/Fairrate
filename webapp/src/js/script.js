cf4343f605839f45912a299119f89c3d5c9565ba
Mukesh0103 <mukeshkannan@users.noreply.github.com>
2020-12-10 20:02:55 +0530

    Page 2 updated

--- webapp/src/js/script.js
+++ webapp/src/js/script.js
@@ -1,16 +1,45 @@
 var screen1 = document.getElementById("screen1");
 var screen2 = document.getElementById("screen2");
-var nextPage = document.getElementById("next-btn");
-nextPage.addEventListener('click', function () {
-    getComputedStyle(screen2).display === "none" ? (screen2.style.display = "block") && (screen1.style.display = "none") : (screen1.style.display = "block");
-})
+var screen3 = document.getElementById("screen3");
+function changeScreen(screen) {
+    if (screen === "screen1") {
+        screen1.style.display = "block";
+        screen2.style.display = "none";
+        screen3.style.display = "none";
+        document.getElementById("home").classList.add("active");
+        document.getElementById("application").classList.remove("active");
+        document.getElementById("documents").classList.remove("active");
+    } else if (screen === "screen2") {
+        screen1.style.display = "none";
+        screen2.style.display = "block";
+        screen3.style.display = "none";
+        document.getElementById("application").classList.add("active");
+        document.getElementById("home").classList.remove("active");
+        document.getElementById("documents").classList.remove("active");
+    } else {
+        screen1.style.display = "none";
+        screen2.style.display = "none";
+        screen3.style.display = "block";
+        document.getElementById("documents").classList.add("active");
+        document.getElementById("home").classList.remove("active");
+        document.getElementById("application").classList.remove("active");
+    }
+
+}
 
 
 var selectDropdown = document.getElementById("select-dropdown");
+var selectBox = document.getElementById("select-box");
 var selectInput = document.getElementById("select-input");
 selectInput.addEventListener('click', function () {
     selectDropdown.classList.toggle("show");
 })
+
+window.addEventListener('click', function (e) {
+    if (!selectBox.contains(e.target)) {
+        selectDropdown.classList.remove("show");
+    }
+});
 
 var dropdownList = document.getElementsByClassName("dropdown-list");
 for (var i = 0; i < dropdownList.length; i++) {
@@ -20,13 +49,3 @@ for (var i = 0; i < dropdownList.length; i++) {
         selectDropdown.classList.remove("show");
     });
 }
-
-// var profileDetails = document.getElementById("screen2");
-// var inputCheckbox = document.getElementsByClassName("checkbox-container");
-// inputCheckbox.addEventListener('change', function(){
-//     if(inputCheckbox.checked === true) {
-//         profileDetails.classList.add("is-selected");
-//     } else {
-//         profileDetails.classList.remove("is-selected")   
-//     }
-// })
